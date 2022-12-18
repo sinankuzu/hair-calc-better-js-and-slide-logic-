e8f03e730ac6ab4a82aba253571e2a62cf1fc773
Sinan <sinankzu@gmail.com>
2022-12-18 20:10:37 +0100

    aa

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const pageThreeWoman = document.querySelector(".woman-page");
 const man = document.querySelector(".man");
 const woman = document.querySelector(".woman");
 const level = document.querySelector(".level-1");
-const pageThree = document.querySelector(".third-page")
+const pageThree = document.querySelector(".third-page");
+const total = document.querySelectorAll(".woman-page .images")
 
 let position = 0;
 let counter = 1;
@@ -17,46 +18,55 @@ let levelPage = 75;
 const next = (e) => {
     slide.style.transform = `translateX(${position - 650}px)`;
      position -= 650;
-     counter++
+    
 
       // Level/page bar filling
 
      level.style.width = `${levelPage + 75}px`;
      levelPage+=75;
 
-     // displays back button
-     if(counter >= 2){
-        previous.style.display = "block"
-    }
-    
+
     // next for second page
 
-    if(counter >= 2){
+    if(counter == 2){
 
-        //It will bring the woman page or man page. man/woman = 2nd page choose with image,
-        //pageThree... = third page will be man or woman due to 2nd page choice.
+      // displays back button
+      previous.style.display = "block";
 
-        if(e.target.classList == "man"){
-            man.classList.add("selected");
-            pageThreeMan.classList.add("show")
-            woman.classList.remove("selected")
-            pageThreeWoman.classList.remove("show")
-            
-            
-        }
-        else if(e.target.classList == ("woman")){
-            woman.classList.add("selected");
-            pageThreeWoman.classList.add("show");
-            man.classList.remove("selected")
-            pageThreeMan.classList.remove("show")
-            
-        }
+
+      //It will bring the woman page or man page. man/woman = 2nd page choose with image,
+      //pageThree... = third page will be man or woman due to 2nd page choice.
+
+      if (e.target.classList == "man") {
+        man.classList.add("selected");
+        pageThreeMan.classList.add("show");
+        woman.classList.remove("selected");
+        pageThreeWoman.classList.remove("show");
+      } else if (e.target.classList == "woman") {
+        woman.classList.add("selected");
+        pageThreeWoman.classList.add("show");
+        man.classList.remove("selected");
+        pageThreeMan.classList.remove("show");
+      }
+      
     }
 
+    if(counter == 3){
+        console.log("yes 3")
+          e.target.classList.add("selected");
+          const x = e.targer.classList;
+         
+          if(e.target.classList != x){
+            
+          }
+          
+    }
 
+counter++;
+ console.log(counter);
 }
 
-// hide back button at the first rendering
+// hide back button at the first page
 
 previous.style.display = "none";
 
@@ -84,10 +94,16 @@ const back = () => {
 }
 }
 
+
+
 // adding on click functions
 
 button.addEventListener("click", next);
 previous.addEventListener("click", back);
 man.addEventListener("click", next)
 woman.addEventListener("click", next)
+// recedingHairline.addEventListener("click", next);
 
+total.forEach((e)=>{
+    e.addEventListener("click",next)
+})
