501307abd0ec23141524b228aa1b975dc4df9625
Sinan <sinankzu@gmail.com>
2022-12-22 01:28:31 +0100

    kadin sac problem yeri divlerden img lere tasindi

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const level = document.querySelector(".level-1");
 const pageThree = document.querySelector(".third-page");
 const womanReceding = document.querySelector(".receding-hairline");
 const womanTonsure = document.querySelector(".tonsure");
-const womanLight = document.querySelector(".light-hair")
-const total = document.querySelectorAll(".woman-page .images div");
+const womanLight = document.querySelector(".light-hair");
+const womanProblem = document.querySelectorAll(".woman-page .images img");
 let position = 0;
 let counter = 1;
 let levelPage = 75;
@@ -18,109 +18,84 @@ let levelPage = 75;
 // next slide/page
 
 const next = (e) => {
-    slide.style.transform = `translateX(${position - 650}px)`;
-     position -= 650;
-    
+  slide.style.transform = `translateX(${position - 650}px)`;
+  position -= 650;
 
-      // Level/page bar filling
+  // Level/page bar filling
 
-     level.style.width = `${levelPage + 75}px`;
-     levelPage+=75;
+  level.style.width = `${levelPage + 75}px`;
+  levelPage += 75;
 
+  // next for second page
 
-    // next for second page
+  if (counter == 2) {
+    // displays back button
+    previous.style.display = "block";
 
-    if(counter == 2){
+    //It will bring the woman page or man page. man/woman = 2nd page choose with image,
+    //pageThree... = third page will be man or woman due to 2nd page choice.
 
-      // displays back button
-      previous.style.display = "block";
+    if (e.target.classList == "man") {
+      man.classList.add("selected");
+      pageThreeMan.classList.add("show");
+      woman.classList.remove("selected");
+      pageThreeWoman.classList.remove("show");
+    } else if (e.target.classList == "woman") {
+      woman.classList.add("selected");
+      pageThreeWoman.classList.add("show");
+      man.classList.remove("selected");
+      pageThreeMan.classList.remove("show");
+    }
+  }
 
-
-      //It will bring the woman page or man page. man/woman = 2nd page choose with image,
-      //pageThree... = third page will be man or woman due to 2nd page choice.
-
-      if (e.target.classList == "man") {
-        man.classList.add("selected");
-        pageThreeMan.classList.add("show");
-        woman.classList.remove("selected");
-        pageThreeWoman.classList.remove("show");
-      } else if (e.target.classList == "woman") {
-        woman.classList.add("selected");
-        pageThreeWoman.classList.add("show");
-        man.classList.remove("selected");
-        pageThreeMan.classList.remove("show");
-        console.log(e.target.classList)
+  if (counter == 3) {
+    womanProblem.forEach((element) => {
+      if (e.target.classList == element.classList.value) {
+        element.classList.add("selected");
       }
-      
-    }
- 
-    if(counter == 3){
-        console.log(e.target.className)
-         console.log(womanReceding.className)
-          if (e.target.className == "receding-hairline") {
-            womanReceding.classList.add("selected");
-            // womanTonsure.classList.remove("selected");
-            // womanLight.classList.remove("selected");
-            // console.log("gir");
-          } else if (e.target.classList === ".tonsure") {
-            womanTonsure.classList.add("selected");
-            womanReceding.classList.remove("selected");
-            womanLight.classList.remove("selected");
-            console.log("gir2");
-          } else if (e.target.classList === ".light-hair") {
-            womanLight.classList.add("selected");
-            womanReceding.classList.remove("selected");
-            womanTonsure.classList.remove("selected");
-            console.log("gir3");
-          }
-        
+      if (e.target.classList != element.classList.value) {
+        element.classList.remove("selected");
+      }
+    });
+  }
 
-    }
-
-counter++;
-console.log(counter)
- 
-}
+  counter++;
+};
 
 // hide back button at the first page
 
 previous.style.display = "none";
 
-
 // previous slide/ page
 
 const back = () => {
-
-    if(counter >= 2){
-        slide.style.transform = `translateX(${position + 650}px)`;
+  if (counter >= 2) {
+    slide.style.transform = `translateX(${position + 650}px)`;
     position += 650;
     counter--;
 
     // Level/page bar decreasing
 
     level.style.width = `${levelPage - 75}px`;
-    levelPage-=75;
+    levelPage -= 75;
 
     // Hide back button if the user returned to first page
 
-    if(counter == 1){
-        previous.style.display = "none";
-        level.style.width =  "75px";
-}
-}
-}
-
-
+    if (counter == 1) {
+      previous.style.display = "none";
+      level.style.width = "75px";
+    }
+  }
+};
 
 // adding on click functions
 
 button.addEventListener("click", next);
 previous.addEventListener("click", back);
-man.addEventListener("click", next)
-woman.addEventListener("click", next)
+man.addEventListener("click", next);
+woman.addEventListener("click", next);
 // recedingHairline.addEventListener("click", next);
 
-total.forEach((e)=>{
-    e.addEventListener("click", next);
-})
-
+womanProblem.forEach((e) => {
+  e.addEventListener("click", next);
+});
